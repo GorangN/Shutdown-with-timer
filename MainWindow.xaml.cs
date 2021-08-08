449de08d2cbfc274ffc43a2e4a55f6ac0d7415f7
GorangN <gorangnagpal@gmail.com>
2021-08-08 16:25:05 +0200

    MahApps Metro hinzugefügt und MahApps IconPacks hinzugefügt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,13 +13,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using ControlzEx.Theming;
 
 namespace Shutdown_with_timer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         #region Zeitangabe
         //public int Zeit
