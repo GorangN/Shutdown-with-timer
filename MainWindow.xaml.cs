b7f531503c65585f98d6782aef95927177ed76a3
Gorang Nagpal <g.nagpal@hawe.de>
2021-10-19 16:47:26 +0200

    ComboBox, Refactoring

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,26 +1,14 @@
-﻿using System;
-using System.Diagnostics;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
-using ControlzEx.Theming;
-
-namespace Shutdown_with_timer
+﻿namespace Shutdown_with_timer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    using MahApps.Metro.Controls;
+
     public partial class MainWindow : MetroWindow
     {
         #region Zeitangabe
@@ -40,9 +28,83 @@ namespace Shutdown_with_timer
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Shutdown(sender, e);
-            this.Close();
-            MessageBox.Show($"The PC will shutdown in {TextBox_Timer.Text} minutes");
+            string value = TypeOfDestruction();
+            this.WindowState = WindowState.Minimized;
+            if (this.WindowState == WindowState.Minimized)
+            {
+                Hide();
+                //TODO: Minimize to System Tray
+            }
+            MessageBox.Show($"The PC will {value} in {TextBox_Timer.Text} minutes \n\n Confirm with OK: ");
+            ExecuteCommand(sender, e, value);
+        }
+
+        private string TypeOfDestruction()
+        {
+            string value = "Error";
+            if (comboBox.SelectedItem == shutdown)
+            {
+                value = "shutdown";
+            }
+            else if (comboBox.SelectedItem == sleep)
+            {
+                value = "sleep";
+            }
+            else if (comboBox.SelectedItem == lockdesktop)
+            {
+                value = "lockScreen";
+            }
+            return value;
+        }
+        private void ExecuteCommand(object sender, RoutedEventArgs e, string value)
+        {
+            if (value == "shutdown")
+            {
+                Shutdown(sender, e);
+            }
+            else if (value == "sleep")
+            {
+                Sleep(sender, e);
+            }
+            else if (value == "lockScreen")
+            {
+                LockScreen(sender, e);
+            }
+        }
+
+        #region TypeOfExecution
+        private void LockScreen(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TextBox_Timer.Text, out _))
+            {
+                string input = TextBox_Timer.Text;
+                if ((input?.Length) != 0)
+                {
+                    if (int.TryParse(input, out int timer))
+                    {
+                        timer *= 1000 * 60;
+                        System.Threading.Thread.Sleep(timer);
+                        Command("Rundll32.exe user32.dll,LockWorkStation");
+                    }
+                }
+            }
+        }
+
+        private void Sleep(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TextBox_Timer.Text, out _))
+            {
+                string input = TextBox_Timer.Text;
+                if ((input?.Length) != 0)
+                {
+                    if (int.TryParse(input, out int timer))
+                    {
+                        timer *= 1000 * 60;
+                        System.Threading.Thread.Sleep(timer);
+                        Command("rundll32.exe powrprof.dll, SetSuspendState Sleep");
+                    }
+                }
+            }
         }
 
         private void Shutdown(object sender, RoutedEventArgs e)
@@ -51,14 +113,17 @@ namespace Shutdown_with_timer
             if (timeconvert)
             {
                 string input = TextBox_Timer.Text;
-                if (input?.Length == 0)
+                if ((input?.Length) != 0)
                 {
-                    Shutdown(sender, e);
-                }
-                else if (int.TryParse(input, out int timer))
-                {
-                    timer *= 60;
-                    Command($"shutdown -s -t {timer}");
+                    if (int.TryParse(input, out int timer))
+                    {
+                        timer *= 60;
+                        Command($"shutdown -s -t {timer}");
+                    }
+                    else
+                    {
+                        Shutdown(sender, e);
+                    }
                 }
                 else
                 {
@@ -70,6 +135,7 @@ namespace Shutdown_with_timer
                 TextBox_Timer.Text.DefaultIfEmpty();
             }
         }
+        #endregion
 
         public void Command(string input)
         {
@@ -84,29 +150,10 @@ namespace Shutdown_with_timer
             process.StandardInput.Flush();
             process.StandardInput.Close();
         }
-
         private void TextBox_Timer_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             var textbox = sender as TextBox;
             textbox.Clear();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
         }
     }
 }
