8c03c968ed8fde137311310bbfb18e330c19c8fe
Gorang Nagpal <82958152+GorangN@users.noreply.github.com>
2025-07-06 03:33:02 +0200

    ✨ Implement automatic theme switching based on Windows system setting - Detects system light/dark mode at startup - Subscribes to Windows theme changes via UserPreferenceChanged - Applies MahApps theme accordingly in real time - Removed static RaiseThemeChanged method and event (unused)

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Win32;
+
 
 namespace Shutdown_with_timer
 {
@@ -15,6 +17,50 @@ namespace Shutdown_with_timer
     public partial class App : Application
     {
         public static event EventHandler<Theme> ThemeChanged;
-    public static void RaiseThemeChanged(Theme newTheme) => ThemeChanged?.Invoke(null, newTheme);
+        public static void RaiseThemeChanged(Theme newTheme) => ThemeChanged?.Invoke(null, newTheme);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ApplySystemTheme();
+            SystemEvents.UserPreferenceChanged += OnUserPreferenceChanged;
+        }
+
+        private void OnUserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category == UserPreferenceCategory.General)
+            {
+                ApplySystemTheme();
+            }
+        }
+
+        private void ApplySystemTheme()
+        {
+            bool isDark = IsWindowsInDarkMode();
+            var themeName = isDark ? "Dark.Blue" : "Light.Blue";
+            ThemeManager.Current.ChangeTheme(Application.Current, themeName);
+            RaiseThemeChanged(ThemeManager.Current.DetectTheme(Application.Current));
+        }
+
+        private bool IsWindowsInDarkMode()
+        {
+            try
+            {
+                using var key = Registry.CurrentUser.OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize");
+                if (key?.GetValue("AppsUseLightTheme") is int value)
+                    return value == 0;
+            }
+            catch { }
+
+            return false;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            SystemEvents.UserPreferenceChanged -= OnUserPreferenceChanged;
+            base.OnExit(e);
+        }
     }
+
 }
