37869a139e29037f7fbe71db25a4c6053a273e63
Gorang Nagpal <82958152+GorangN@users.noreply.github.com>
2025-07-06 04:22:05 +0200

    Add IntToStringConverter for two-way binding between integer and string in timer input

---
+++ IntToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace Shutdown_with_timer
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            if (int.TryParse(str, out int result))
+                return result;
+
+            return DependencyProperty.UnsetValue; // verhindert Binding-Fehler
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,29 +4,10 @@
     using MahApps.Metro.Controls;
     using System;
     using System.Diagnostics;
-    using System.Globalization;
     using System.Linq;
     using System.Windows;
-    using System.Windows.Data;
     using System.Windows.Forms;
     using System.Windows.Input;
-
-    public class IntToStringConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value?.ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var str = value as string;
-            if (int.TryParse(str, out int result))
-                return result;
-
-            return DependencyProperty.UnsetValue; // verhindert Binding-Fehler
-        }
-    }
 
     public partial class MainWindow : MetroWindow
     {
